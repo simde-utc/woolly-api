328eff72db5f9019ccc6b975310c78c163d2dd00
Thomas Barizien <thomas.barizien@gmail.com>
2017-05-09 18:33:53 +0200

    Add model classes basic structure
    Add most basic model attributes, length of CharField maybe to be reconsidered,
    TextField to be considered for some of them
    
    Missing a few attributes like enum types(order)
    Missing custom save methods for certain tables(order)
    Missing user foreign keys to association and WoollyUserType

---
+++ woolly_api/core/models/associationMember.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from core.models.association import Association
+
+
+class AssociationMember(models.Model):
+	user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+	association = models.ForeignKey(Association, on_delete=models.CASCADE)
+	role = models.CharField(max_length=50)
+	rights = models.CharField(max_length=50)
