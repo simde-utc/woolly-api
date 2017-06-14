6cde18375c5b989c3cc14c497dcd58b4b065d2bf
Thomas Barizien <thomas.barizien@gmail.com>
2017-06-14 10:18:41 +0200

    Fix IsOwner permission (return True when GET/HEAD/OPTIONS request)

--- woolly_api/sales/permissions.py
+++ woolly_api/sales/permissions.py
@@ -11,7 +11,7 @@ class IsOwner(permissions.BasePermission):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
-            return False
+            return True
 
         # Instance must have an attribute named `owner`.
         return obj.owner == request.user
