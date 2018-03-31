6138427b4609e95b5622e2d9578093707ee16d5c
Alex <abrasseur.pro@gmail.com>
2018-03-31 22:43:05 +0200

    Add confidential settings

--- payutc/views.py
+++ payutc/views.py
@@ -3,6 +3,8 @@ from django.core import serializers
 import json
 from . import payutc2
 from django.contrib.sessions.backends.db import SessionStore
+from woolly_api import settings
+
 
 def createTransaction(request):
 	#for key, value in request.session.items(): 
@@ -14,7 +16,7 @@ def createTransaction(request):
 	articles = request.body.decode('utf-8')
 	#print(articles)
 	#print("here2")
-	params = {"apikey":"3d85d0bff8009579d45377dd7d49be14"}
+	params = { "apikey": PAYUTC_KEY }
 	p = payutc2.Payutc(params)
 	data  =  {"itemsArray":articles,"funId": funId,"mail" :mail,"returnUrl":"https://payutc.nemopay.net/","callbackUrl" : "http://localhost:8000/payutc/returnTransaction?&funId="+funId+"orderlineId="+orderlineId,"apikey":"d0ba09483a9ed6262d3924d7e7567d37"}
 	response =  json.loads(p.createTransaction(data))
@@ -23,7 +25,7 @@ def createTransaction(request):
 	return JsonResponse(response)
 
 def setProduct(request):
-	params = {"apikey":"3d85d0bff8009579d45377dd7d49be14"}
+	params = { "apikey": PAYUTC_KEY }
 	name = request.GET['name']
 	category = request.GET["category"]
 	price = request.GET['price']
@@ -38,7 +40,7 @@ def setProduct(request):
 	return HttpResponse(response)
 
 def getFundations(request):
-	params = {"apikey":"3d85d0bff8009579d45377dd7d49be14"}
+	params = { "apikey": PAYUTC_KEY }
 	service = request.GET['service']
 	ticket = request.GET['ticket']
 	p = payutc2.Payutc(params)
@@ -47,7 +49,7 @@ def getFundations(request):
 	return HttpResponse(response)
 
 def returnTransaction(request):
-	params = {"apikey":"3d85d0bff8009579d45377dd7d49be14"}
+	params = { "apikey": PAYUTC_KEY }
 	funId = request.GET['funId']
 	oderlineId = request.GET['orderlineId']
 	transactionId = request.query
---
+++ woolly_api/settings_confidential.example.py
@@ -0,0 +1,14 @@
+# SECRET KEYS
+SECRET_KEY = ""
+GINGER_KEY = ""
+PAYUTC_KEY = ""
+
+# MAIN DATABASE
+DATABASE = {
+	'ENGINE': 'django.db.backends.mysql',	# Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+	'NAME': 'woolly',						# Or path to database file if using sqlite3.
+	'USER': 'root',							# Not used with sqlite3.
+	'PASSWORD': '',							# Not used with sqlite3.
+	'HOST': '',								# Set to empty string for localhost. Not used with sqlite3.
+	'PORT': '',								# Set to empty string for default. Not used with sqlite3.
+}
