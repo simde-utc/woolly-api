a4ea98f1403401ae4178ef4076e6d26c500d40eb
Alex <abrasseur.pro@gmail.com>
2018-05-27 15:59:56 +0200

    Add Payutc.filterParams to improve readability

--- payutc/payutc.py
+++ payutc/payutc.py
@@ -7,35 +7,56 @@ class Payutc:
 
 	def __init__(self, params):
 		self.config = {
-			'url': 'https:/api.nemopay.net',
-			'username': 'username',
-			'password': 'password',
+			'username': None,
+			'password': None,
 			'systemID': 'payutc',
 			'async': False,
-			'app_key': params['apikey'],
-			'fun_id': 2,
-			'sessionID': 0,
-			'logged_usr': '',
+			'app_key': params.get('app_key', None),
+			'fun_id': params.get('fun_id', None),
+			'sessionID': None,
+			'logged_usr': None,
 			'loginMethod': 'payuser',
 			'date': date.today(),
-			'debug': False
+			'debug': False,
+			# 'callback_url': params.get('callback_url', None)
 		}
+
+	def filterParams(self, params, keys):
+		"""
+		Filter params to only return keys
+		"""
+		data = { k: params.get(k, None) for k in keys }
+		# Default values
+		if 'fun_id' in keys and data['fun_id'] == None:
+			data['fun_id'] = self.config['fun_id']
+		return data
 
 	def genericApiCall(self, service, method, data):
 		response = ""
-		url = "https://api.nemopay.net/services/" + service + "/" + method + "?system_id=" + self.config["systemID"] + "&app_key=" + self.config["apikey"]
-		if self.config["sessionID"] != 0:
+		url = "https://api.nemopay.net/services/" + service + "/" + method \
+				+ "?system_id=" + self.config["systemID"] + "&app_key=" + self.config["app_key"]
+		if self.config["sessionID"] != None:
 				url += "&sessionid=" + self.config["sessionID"]
 
-		data = json.dumps(data)
-		response = requests.post(url, data=data, headers={"Content-Type": "application/json"})
+		response = requests.post(url, data=json.dumps(data), headers={"Content-Type": "application/json"})
 		return response.text
 
+
+
+	# ============================================
+	# 	Web Transactions
+	# ============================================
+
 	def createTransaction(self, params):
-		return self.genericApiCall("WEBSALE", "createTransaction", {"items": params["itemsArray"], "mail": params["mail"], "return_url": params["returnUrl"], "fun_id": params["funId"], "callback_url": params["callbackUrl"]})
+		keys = ('items', 'mail', 'return_url', 'fun_id', 'callback_url')
+		data = self.filterParams(params, keys)
+		return self.genericApiCall("WEBSALE", "createTransaction", data)
 
 	def getTransactionInfo(self, params):
-		return self.genericApiCall("WEBSALE", "getTransactionInfo", {"tra_id": params["traId"], "fun_id": params["funId"]})
+		data = self.filterParams(params, ('tra_id', 'fun_id'))
+		return self.genericApiCall("WEBSALE", "getTransactionInfo", data)
+
+
 
 	def payuser(self, params): # Cette methode se trouve dans tous les services
 		resp = self.genericApiCall("WEBSALE", "login2", {"login": params["login"], "password": params["password"]})
@@ -43,8 +64,11 @@ class Payutc:
 		self.config["sessionID"] = resp["sessionid"]
 		self.config["logged_usr"] = resp["username"]
 
-	def setProduct(self,params):
-		return self.genericApiCall("GESARTICLE", "setProduct", {"name":params["name"], "parent": params["category"], "prix": params["price"], "stock":params["stock"], "alcool": params["alcool"], "image": params["image"], "image_path": params["image_path"], "fun_id":params["funId"], "obj_id": params["objId"], "tva":params["tva"], "cotisant": params["cotisant"]})
+	def setProduct(self, params):
+		keys = ('name', 'parent', 'prix', 'stock', 'alcool', 'image',
+				'image_path', 'fun_id', 'obj_id', 'tva', 'cotisant')
+		data = self.filterParams(params, keys)
+		return self.genericApiCall("GESARTICLE", "setProduct", data)
 
 	def getAllMyRights(self):
 		return self.genericApiCall("USERRIGHT", "getAllMyRights", {}) # ?? USERRIGHT n'est plus dans l'API
