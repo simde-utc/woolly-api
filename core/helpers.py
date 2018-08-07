56e0a236510696402418f6c9f0eb66d73db2103f
Alex <abrasseur.pro@gmail.com>
2018-08-07 16:00:26 +0200

    Add custom_editable_fields helper
    Useful for admin site to make a difference between creation and edition with readonly fields

--- core/helpers.py
+++ core/helpers.py
@@ -7,3 +7,10 @@ def errorResponse(message, errors = tuple(), httpStatus = status.HTTP_400_BAD_RE
 		'errors': [ {'detail': e} for e in errors ]
 	}
 	return JsonResponse(resp, status=httpStatus)
+
+
+def custom_editable_fields(request, obj=None, edition_readonly_fields=tuple(), always_readonly_fields=tuple()):
+	"""
+	Helper to allow non editable fields to be set on creation
+	"""
+	return edition_readonly_fields if obj else always_readonly_fields
