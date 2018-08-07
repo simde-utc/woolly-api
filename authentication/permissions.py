54a0c5baf1f6e24899f7aa842ad0a7853754ce86
Alex <abrasseur.pro@gmail.com>
2018-08-07 18:30:51 +0200

    Add permissions to authentication views

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -1,14 +1,30 @@
 from rest_framework import permissions
 from .models import User, UserType
 
+class IsOwner(permissions.BasePermission):
+	"""
+	Custom permission class to allow only order owners to edit them.
+	"""
+	def has_permission(self, request, view):
+		return request.user.is_authenticated
 
-class IsUser(permissions.BasePermission):
-	"""Custom permission class to allow only order owners to edit them."""
-	# def has_permission(self, request, view):
-		# return False
 	def has_object_permission(self, request, view, obj):
-		"""Return True if permission is granted to the order owner."""
 		if isinstance(obj, User):
 			return obj == request.user
+		return obj.owner == request.user
 
-		return obj.user == request.user
+class IsOwnerOrAdmin(permissions.BasePermission):
+	"""
+	Custom permission class to allow only order owners and admin to edit them.
+	"""
+	def has_permission(self, request, view):
+		return request.user.is_authenticated
+
+	def has_object_permission(self, request, view, obj):
+		if request.user.is_admin:
+			return True
+
+		if isinstance(obj, User):
+			print(obj == request.user)
+			return obj == request.user
+		return obj.owner == request.user
