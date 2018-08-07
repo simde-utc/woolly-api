24a372006f3646017068b89111e32aa2f06989f1
Alex <abrasseur.pro@gmail.com>
2018-08-07 21:20:32 +0200

    Create CustomPermission to DRY permissions

--- core/permissions.py
+++ core/permissions.py
@@ -1,7 +1,51 @@
 from rest_framework import permissions
 
 
-class IsAdmin(permissions.BasePermission):
+class CustomPermission(permissions.BasePermission):
+	require_authentication = False
+	allow_admin = False
+	allow_read_only = False
+
+	permission_functions = []
+	object_permission_functions = []
+	check_with_or = True
+
+	def _check_functions(function_list):
+		permission_gen = (f(request, view) for f in function_list)
+		return any(permission_gen) if self.check_with_or else all(permission_gen)
+
+	def _is_not_authenticated(self, request, view):
+		return not request.user.is_authenticated
+
+	def has_permission(self, request, view):
+		# Is Authenticated option
+		if self.require_authentication and self._is_not_authenticated:
+			return False
+
+		# Read Only option
+		if self.allow_read_only and request.method in permissions.SAFE_METHODS:
+			return True
+
+		# Check all permission_functions
+		if self.permission_functions:
+			return self._check_functions(self.permission_functions)
+
+		return True
+
+	def has_object_permission(self, request, view, obj):
+		# Allow admin option
+		if self.allow_admin and request.user.is_admin:
+			return True
+
+		# Check all permission_functions
+		if self.object_permission_functions:
+			return self._check_functions(self.object_permission_functions)
+
+		return True
+
+
+
+class IsAdmin(CustomPermission):
 	"""Only Woolly admins have the permission"""
 	def has_permission(self, request, view):
 		return request.user.is_authenticated and request.user.is_admin
@@ -16,7 +60,7 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 class IsAdminOrAuthenticatedReadOnly(permissions.BasePermission):
 	"""Only Woolly admins have the permission to modify, authenticated users can read"""
 	def has_permission(self, request, view):
-		if request.user.is_authenticated == False:
+		if not request.user.is_authenticated:
 			return False
 		if request.method in permissions.SAFE_METHODS:
 			return True
--- sales/permissions.py
+++ sales/permissions.py
@@ -1,55 +1,38 @@
 from rest_framework import permissions
-from .models import Order, OrderLine, OrderLineItem, OrderLineField
+from core.permissions import CustomPermission
+from .models import *
 
 
-class IsOwner(permissions.BasePermission):
-	"""Custom permission class to allow only order owners to edit them."""
-	def has_object_permission(self, request, view, obj):
-		"""Return True if permission is granted to the order owner."""
+def check_ownership(request, view, obj):
+	if isinstance(obj, Order):
+		return obj.owner == request.user
+	if isinstance(obj, OrderLine):
+		return obj.order.owner == request.user
+	if isinstance(obj, OrderLineItem):
+		return obj.orderline.order.owner == request.user
+	if isinstance(obj, OrderLineField):
+		return obj.orderitem.orderline.order.owner == request.user
 
-		if isinstance(obj, Order):
-			return obj.owner == request.user
-		if isinstance(obj, OrderLine):
-			return obj.order.owner == request.user
-		if isinstance(obj, OrderLineItem):
-			return obj.orderline.order.owner == request.user
-		if isinstance(obj, OrderLineField):
-			return obj.orderitem.orderline.order.owner == request.user
+class IsOwner(CustomPermission):
+	require_authentication = True
+	object_permission_functions = (check_ownership,)
 
-		return obj.user == request.user
+class IsOwnerOrAdmin(IsOwner):
+	allow_admin = True
+
+class IsOwnerOrReadOnly(IsOwner):
+	allow_read_only = True
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
-	"""
-	Object-level permission to only allow owners of an object to edit it.
-	Assumes the model instance has an `owner` attribute.
-	"""
-	def has_object_permission(self, request, view, obj):
-		if request.method in permissions.SAFE_METHODS:
-			return True
+# TODO
+def check_manager(request, view, obj):
+	return request.user.is_admin
 
-		if isinstance(obj, Order):
-			return obj.owner == request.user
-		if isinstance(obj, OrderLine):
-			return obj.order.owner == request.user
-		if isinstance(obj, OrderLineItem):
-			return obj.orderline.order.owner == request.user
-		if isinstance(obj, OrderLineField):
-			return obj.orderitem.orderline.order.owner == request.user
 
-		return obj.user == request.user
+class IsManager(CustomPermission):
+	require_authentication = True
+	object_permission_functions = (check_manager,)
 
-class IsManager(permissions.BasePermission):
-	"""Only assos' manager have the permission to modify"""
-	def has_permission(self, request, view):
-		# TODO
-		return request.user.is_authenticated and request.user.is_admin
-
-class IsManagerOrReadOnly(permissions.BasePermission):
-	"""Only assos' manager have the permission to modify, anyone can read"""
-	def has_permission(self, request, view):
-		if request.method in permissions.SAFE_METHODS:
-			return True
-		# TODO
-		return request.user.is_authenticated and request.user.is_admin
+class IsManagerOrReadOnly(CustomPermission):
+	allow_read_only = True
 
