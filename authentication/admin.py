0da573d2d2eec9b094d213fc9bbf0e2a6f94a48c
Alex <abrasseur.pro@gmail.com>
2018-08-07 16:32:13 +0200

    Clear core.admin & Add associations to UserAdmin

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from authentication.models import User, UserType
-from authentication.forms import UserCreationForm, UserChangeForm
-
+from .models import User, UserType
+from .forms import UserCreationForm, UserChangeForm
+from sales.admin import AssociationMemberInline
 
 class UserAdmin(BaseUserAdmin):
 	form = UserChangeForm
@@ -14,6 +14,7 @@ class UserAdmin(BaseUserAdmin):
 	list_filter = ('is_admin', 'usertype')
 	list_editable = tuple()
 
+	inlines = (AssociationMemberInline,)
 	fieldsets = (
 		(None, { 'fields': ('email', 'password', 'first_name', 'last_name', 'usertype') }),
 		('Permissions', { 'fields': ('is_admin',) }),
