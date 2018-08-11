fe5305ac817c0cebc90164428542f130ffa8df6b
Alex <abrasseur.pro@gmail.com>
2018-08-11 18:00:30 +0200

    Comment CRUDViewSetTestMixin

--- core/tests.py
+++ core/tests.py
@@ -23,11 +23,15 @@ VISIBILITY_SHORTCUTS = {
 
 
 def debug_permissions(permissions):
+	"""Helper to pretty print permissions"""
 	for action in permissions:
 		string_list = (perm for perm in permissions[action] if permissions[action][perm] is True)
 		print(action, ":\t\t" if len(action) < 6 else ":\t", ', '.join(string_list))
 
 def get_permissions_from_compact(compact):
+	"""
+	Helper to get a complete permission list from a compact one { 'list': "puoa", 'delete': ".u.a" }
+	"""
 	# Important ! https://www.peterbe.com/plog/be-careful-with-using-dict-to-create-a-copy
 	permissions = deepcopy(DEFAULT_CRUD_PERMISSIONS)
 	for action in compact:
@@ -44,14 +48,20 @@ class CRUDViewSetTestMixin(object):
 	debug = False
 
 	def setUp(self):
+		"""Function run before beginning the tests"""
+
+		# resource_name MUST be specified
 		if not self.resource_name:
 			raise NotImplementedError("Please specify the resource_name")
+
 		# Get users
 		self.users = {
 			'admin': User.objects.create_superuser(email="admin@woolly.com"),
 			'user': User.objects.create_user(email="user@woolly.com"),
 			'other': User.objects.create_user(email="other@woolly.com"),
 		}
+
+		# Create test object
 		self.object = self._create_object()
 
 		# Debug
@@ -67,10 +77,12 @@ class CRUDViewSetTestMixin(object):
 	# ========================================================
 
 	def _is_allowed(self, action, visibility):
+		"""Helper to know if action is allowed with specified visibility"""
 		default = DEFAULT_CRUD_PERMISSIONS[action].get(visibility, False)
 		return self.permissions[action].get(visibility, default)
 
 	def _get_url(self, pk=None):
+		"""Helper to get url from resource_name and pk"""
 		try:
 			if pk is None:
 				return reverse(self.resource_name + '-list')
@@ -80,19 +92,30 @@ class CRUDViewSetTestMixin(object):
 			self.assertIsNotNone(url, "Route '%s' is not defined" % route['name'])
 
 	def _test_user_permission(self, url, user=None, allowed=True, **kwargs):
+		"""
+		@brief   Helper to make the user try to access the url with specified method
+		@param   url                   The url to access
+		@param   user                  The user which does the request
+		@param   allowed               Whether the user should be allowed to perform the request
+		@param   method                The HTTP method used to access the url
+		@param   data                  The data to bind to the HTTP request
+		@param   expected_status_code  The status code that should be returned by the request
+		"""
 		# Authenticate with specified user
 		self.client.force_authenticate(user = self.users.get(user, None))
 
-		# Get url and check code
+		# Create request
 		call_method = getattr(self.client, kwargs.get('method', 'get'))
 		response = call_method(url, kwargs.get('data', None), format='json')
 
 		expected_status_code = kwargs.get('expected_status_code', status.HTTP_200_OK if allowed else status.HTTP_403_FORBIDDEN)
 		if self.debug:
-			print(" Expected permission for '" + (user or 'public') +"': ", allowed)
+			print(" Expected status_code for '" + (user or 'public') +"': ", expected_status_code)
+
 		self.assertEqual(response.status_code, expected_status_code, "for '%s' user" % user)
 
 	def _create_object(self, user=None):
+		"""Method used to create the initial object with no user and then the per-user objects, must be overriden"""
 		raise NotImplementedError("This function must be overriden")
 
 
@@ -101,6 +124,7 @@ class CRUDViewSetTestMixin(object):
 	# ========================================================
 
 	def test_list_view(self):
+		"""Test all users permissions to list"""
 		url = self._get_url()
 		if self.debug:
 			print("\n== Begin '" + self.resource_name + "' list view test")
@@ -112,6 +136,7 @@ class CRUDViewSetTestMixin(object):
 		self._test_user_permission(url, None, 		self._is_allowed('list', 'public'))
 
 	def test_retrieve_view(self):
+		"""Test all users permissions to retrieve self.object"""
 		url = self._get_url(pk = self.object.pk)
 		if self.debug:
 			print("\n== Begin '" + self.resource_name + "' retrieve view test")
@@ -124,6 +149,7 @@ class CRUDViewSetTestMixin(object):
 		self._test_user_permission(url, None, 		self._is_allowed('retrieve', 'public'))
 
 	def test_create_view(self):
+		"""Test all users permissions to create an object"""
 		url = self._get_url()
 		if self.debug:
 			print("\n== Begin '" + self.resource_name + "' create view test")
