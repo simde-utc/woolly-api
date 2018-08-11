187aacd3e86fbdea33814976237d9de748286c6d
Alex <abrasseur.pro@gmail.com>
2018-08-11 14:01:34 +0200

    Begin CRUDViewSetTestMixin

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from core.tests import CRUDViewSetTestMixin
+from authentication.models import *
 
-# Create your tests here.
+class UserViewSetTestCase(CRUDViewSetTestMixin, APITestCase):
+	resource_name = 'user'
+	route_visibility = {
+		'public': False,
+		'authenticated': False
+	}
+
+
--- core/tests.py
+++ core/tests.py
@@ -4,6 +4,53 @@ from rest_framework.test import APITestCase
 from authentication.models import User
 
 
+class CRUDViewSetTestMixin(object):
+	resource_name = None
+	route_visibility = {
+		'public': False,
+		'authenticated': False
+	}
+
+	def setUp(self):
+		if self.resource_name is None:
+			raise NotImplementedError("Please specify the resource_name")
+		self.users = {
+			'admin': User.objects.create_superuser(email="admin@woolly.com"),
+			'lambda1': User.objects.create_user(email="lambda1@woolly.com"),
+			'lambda2': User.objects.create_user(email="lambda2@woolly.com"),
+		}
+
+	def _get_url(self, pk=None):
+		try:
+			if pk is None:
+				return reverse('user-list')
+				# return reverse(self.resource_name + '-list'),
+			else:
+				return reverse(self.resource_name + '-detail', { 'pk': pk })
+		except exceptions.NoReverseMatch:
+			self.assertIsNotNone(url, "Route '%s' is not defined" % route['name'])
+
+	def _test_user_permission(self, url, user = None, allowed = True):
+		# Authenticate with specified user
+		self.client.force_authenticate(user = self.users.get(user, None))
+		# Get url and check code
+		response = self.client.get(url, format='json')
+
+		expected_status_code = status.HTTP_200_OK if allowed else status.HTTP_403_FORBIDDEN
+		# expected_status_code = status.HTTP_401_UNAUTHORIZED if user is None else status.HTTP_403_FORBIDDEN
+
+		self.assertEqual(response.status_code, expected_status_code, "for '%s' user" % user)
+
+	def test_list_view(self):
+		url = self._get_url()
+
+		# Test List permissions
+		self._test_user_permission(url, 'admin', True)
+		self._test_user_permission(url, 'lambda1', self.route_visibility['authenticated'])
+		self._test_user_permission(url, None, self.route_visibility['public'])
+
+
+
 class RoutesTestCase(APITestCase):
 	def setUp(self):
 		self.users = {
@@ -59,5 +106,5 @@ ROUTE_LIST = [
 ]
 
 # Programmaticaly add tests for each routes
-for route in ROUTE_LIST:
-	setattr(RoutesTestCase, 'test_'+route['name']+'_route', gen_route_test(route))
+# for route in ROUTE_LIST:
+	# setattr(RoutesTestCase, 'test_'+route['name']+'_route', gen_route_test(route))
