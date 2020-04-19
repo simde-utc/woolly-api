58fd388e2c85c5badd94e57b18597d53e22b3d10
Alexandre Brasseur <abrasseur@meilleursagents.com>
2020-04-19 18:27:25 +0200

    refactor(permissions): Refactor IsManagerOrReadOnly permission

--- sales/permissions.py
+++ sales/permissions.py
@@ -1,3 +1,5 @@
+from rest_framework import permissions
+
 from core.exceptions import InvalidRequest
 from core.permissions import CustomPermission
 from authentication.oauth import OAuthAPI
@@ -7,103 +9,112 @@ from .models import (
 )
 
 
-# TODO Refactor
-def get_url_param(request, view, name: str):
-    param_request = request.data.get(name)
-    if param_request:
-        return param_request
-    else:
-        raise InvalidRequest(f"Could not retrieve parameter '{name}' from request")
+class IsManagerOrReadOnly(permissions.BasePermission):
+    """
+    Check that a user is manager of the Association
+    related to the target resource
 
-    # TODO Needed ?
-    param_kwargs = view.kwargs.get(f"{name}_pk")
+    Used for Association, Sale, ItemGroup, Item, ItemField
+    """
 
-    # Need one or the other or both equal
-    if param_request:
-        if param_kwargs:
-            if param_request != param_kwargs:
-                raise InvalidRequest(f"Got different parameter values for '{name}' from request")
+    def get_url_param(self, name: str):
+        param_request = self.request.data.get(name)
+        if param_request:
             return param_request
-    elif param_kwargs:
-        return param_kwargs
+        else:
+            raise InvalidRequest(f"Could not retrieve parameter '{name}' from request")
 
-    raise InvalidRequest(f"Could not retrieve parameter '{name}' from request")
+        # TODO Needed ?
+        """
+        param_kwargs = self.view.kwargs.get(f"{name}_pk")
 
+        # Need one or the other or both equal
+        if param_request:
+            if param_kwargs:
+                if param_request != param_kwargs:
+                    raise InvalidRequest(f"Got different parameter values for '{name}' from request")
+                return param_request
+        elif param_kwargs:
+            return param_kwargs
 
-def get_related_model(Model, pk, *related):
-    try:
-        request = Model.objects
-        if related:
-            request = request.select_related(*related)
-        return request.get(pk=pk)
-    except Model.DoesNotExist:
-        raise InvalidRequest(f"Could not retrieve related {Model.__name__}")
+        raise InvalidRequest(f"Could not retrieve parameter '{name}' from request")
+        """
 
+    def get_related_model(self, Model, pk, *related):
+        try:
+            query = Model.objects
+            if related:
+                query = query.select_related(*related)
+            return query.get(pk=pk)
+        except Model.DoesNotExist:
+            raise InvalidRequest(f"Could not retrieve related {Model.__name__}")
 
-def get_related_asso_id(request, view) -> str:
-    Model = view.queryset.model
-    if Model not in { Association, Sale, Item, ItemGroup, ItemField }:
-        raise NotImplementedError(f"Model {Model} is not managed")
+    def get_related_asso_id(self, request, view) -> str:
+        Model = view.queryset.model
+        if Model not in { Association, Sale, Item, ItemGroup, ItemField }:
+            raise NotImplementedError(f"Model {Model} is not managed")
 
-    if Model == Association:
-        if view.action in {'create', 'delete'}:
+        hasPk = 'pk' in view.kwargs
+        pk = view.kwargs.get('pk')
+
+        if Model == Association:
+            if view.action in {'create', 'delete'}:
+                return False
+            else:
+                return view.kwargs.get('pk')
+
+        elif Model == Sale:
+            if hasPk:
+                return self.get_related_model(Sale, pk).association_id
+            else:
+                return self.get_url_param('association')
+
+        elif Model in { Item, ItemGroup }:
+            if hasPk:
+                return self.get_related_model(Model, pk, 'sale').sale.association_id
+            else:
+                sale_pk = self.get_url_param('sale')
+                return self.get_related_model(Sale, sale_pk).association_id
+
+        elif Model == ItemField:
+            if hasPk:
+                return self.get_related_model(ItemField, pk, 'item__sale').item.sale.association_id
+            else:
+                item_pk = self.get_url_param('item')
+                return self.get_related_model(Item, item_pk, 'sale').sale.association_id
+
+        raise InvalidRequest("Could not retrieve related Association")
+
+    def has_permission(self, request, view) -> bool:
+        # Check view's Model
+        Model = view.queryset.model
+        if Model not in {Association, Sale, Item, ItemGroup, ItemField}:
+            raise NotImplementedError(f"Object {Model} is not managed")
+
+        # Allow read only
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if not request.user.is_authenticated:
             return False
-        else:
-            return view.kwargs.get('pk')
+        if request.user.is_admin:
+            return True
 
-    elif Model == Sale:
-        if 'pk' not in view.kwargs:
-            return get_url_param(request, view, 'association')
-        else:
-            pk = view.kwargs.get('pk')
-            return get_related_model(Sale, pk).association_id
+        # Get the related association
+        asso_id = self.get_related_asso_id(request, view)
 
-    elif Model in { Item, ItemGroup }:
-        if 'pk' not in view.kwargs:
-            sale_pk = get_url_param(request, view, 'sale')
-            return get_related_model(Sale, sale_pk).association_id
-        else:
-            pk = view.kwargs.get('pk')
-            return get_related_model(Model, pk, 'sale').sale.association_id
+        # Check that the association is real
+        if not Association.objects.filter(pk=asso_id).exists():
+            raise InvalidRequest(f"Could not retrieve association '{asso_id}'")
 
-    elif Model == ItemField:
-        if 'pk' not in view.kwargs:
-            item_pk = get_url_param(request, view, 'item')
-            return get_related_model(Item, item_pk, 'sale').sale.association_id
-        else:
-            pk = view.kwargs.get('pk')
-            return get_related_model(ItemField, pk, 'item__sale').item.sale.association_id
+        # Get user's associations and check if is manager
+        oauth_client = OAuthAPI(session=request.session)
+        user = request.user.get_with_api_data_and_assos(oauth_client)
+        return user.is_manager_of(asso_id)
 
-    raise InvalidRequest("Could not retrieve related Association")
-
-
-def check_manager(request, view) -> bool:
-
-    Model = view.queryset.model
-    if Model not in {Association, Sale, Item, ItemGroup, ItemField}:
-        raise NotImplementedError(f"Object {Model} is not managed")
-
-    if not request.user.is_authenticated:
-        return False
-
-    # Get the related association
-    asso_id = get_related_asso_id(request, view)
-
-    # Checl asso_id
-    if not Association.objects.filter(pk=asso_id).exists():
-        raise InvalidRequest(f"Could not retrieve association '{asso_id}'")
-
-    # Get user's associations and check if is manager
-    oauth_client = OAuthAPI(session=request.session)
-    user = request.user.get_with_api_data_and_assos(oauth_client)
-    return user.is_manager_of(asso_id)
-
-
-# Used for Association, Sale, ItemGroup, Item, ItemField
-class IsManagerOrReadOnly(CustomPermission):
-    allow_read_only = True
-    permission_functions = (check_manager,)
-    default_obj = True  # No additional check for object
+    def has_object_permission(self, request, view, obj) -> bool:
+        # No need for object permission
+        return True
 
 
 def check_order_ownership(request, view, obj):
